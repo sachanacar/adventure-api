cd9612ece75f540b41557c2e2877abb4084ada86
Sacha Nacar <sacha.slushy@gmail.com>
2015-07-08 17:11:00 +0200

    added text/plain content type in headers

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,19 @@ var inventory = ['empty', 'empty', 'empty', 'empty', 'empty'];
 var i = inventory.length;
 //Welcome page
 router.get('/welcome', function(req, res) {
+  res.setHeader('content-type', 'text/plain');
   res.send("======================================\n\n  __    __     _ \n / / /\\ \\ \\___| | ___ ___  _ __ ___   ___ \n \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\  \n  \\  /\\  /  __/ | (_| (_) | | | | | |  __/  \n   \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|\n\nYou've trespassed on the API Temple. Prepare to die, or be tickled by lolcatz or something... I am Reginald X, your assistant for the road ahead, but just call me Regex or Reggie. You can always ask for help by calling me at /regex . Start the adventure by entering the temple (/start)\n\n\n======================================\n");
 });
 
 //This is Reggie the assistant
 router.get('/regex', function(req, res) {
+  res.setHeader('content-type', 'text/plain');
   res.send("======================================\n\nI'm Reggie, your faithful assistant. \n\n༼ つ ◕_◕ ༽つ\n\n\n\nInventory:\n==========\n\nYou can use your inventory items by passing them as parameters in a request! (For example, unlocking a door using an access code you picked up becomes: '../door/unlock?code=xxx')\n\n- "+inventory[0]+"\n- "+inventory[1]+"\n- "+inventory[2]+"\n- "+inventory[3]+"\n- "+inventory[4]+"\n\n======================================\n");
 });
 
 //Entrance!
 router.get('/start', function(req, res) {
+	 res.setHeader('content-type', 'text/plain');
   	 res.send("======================================\n\n\n                                  /   \\ \n _                        )      ((   ))     (\n(@)                      /|\\      ))_((     /|\\ \n|-|                     / | \\    (/\\|/\\)   / | \\                      (@)\n| | -------------------/--|-voV---\\`|'/--Vov-|--\\---------------------|-|\n|-|                         '^`   (o o)  '^`                          | |\n| |                               `\\Y/'                               |-|\n|-|                                                                   | |\n| |    You have now entered the temple, you will have to make         |-|\n|-|    decisions that could be fatal at any moment. You have one      | |\n| |    to make now: /west-room or /east-room ?                        |-|\n|_|___________________________________________________________________| |\n(@)              l   /\\ /         ( (       \\ /\\   l                `\\|-|\n                 l /   V           \\ \\       V   \\ l                  (@)\n                 l/                _) )_          \\I\n                                   `\\ /\n\n\n======================================\n")});
 
 //Hall
@@ -32,8 +35,10 @@ if (torch == 'on'){
   if (index !== -1 && codestring ==-1) {
     inventory[index] = 'code:678';
   }
-  res.send("======================================\n\nYou lit up your torch.\n\nOh! seems like there's something in the corner.\n\nIt's a piece of paper with a code on it: '687'. Let's keep it for later...\n\n[Code stored in your inventory!]\n\n======================================\n");
+  res.setHeader('content-type', 'text/plain');
+  res.send("======================================\n\nYou lit up your torch.\n\nOh! seems like there's something in the corner.\n\nIt's a piece of paper with a code on it: '687'.\n\nWhere could you use this? try going back to the /hall \n\n[Code stored in your inventory!]\n\n======================================\n");
 }else{
+  res.setHeader('content-type', 'text/plain');
   res.send("======================================\n\nYou've entered the West Room. It's dark in here...\n\n\n༼ つ ◕_◕ ༽つ\n\n**Reggie**: use your torch! (/west-room?torch=on)\n\n======================================\n");
 }
 });
